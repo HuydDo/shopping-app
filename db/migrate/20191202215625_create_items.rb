36c949137fe6e4317dfe15343a964e491c663ae0
HuydDo <huydinhdo@yahoo.com>
2019-12-02 15:19:04 -0700

    add controller, model for item,  model for cart, update migration cart, item

---
+++ db/migrate/20191202215625_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.text :name
+      t.integer :quantity 
+      t.float :price 
+      t.integer :review
+      t.timestamps
+    end
+  end
+end
