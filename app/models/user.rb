43cc6c38835285777da5efa7f1b1df12a49c2883
HuydDo <huydinhdo@yahoo.com>
2019-12-03 14:48:59 -0700

    modify model for item, order, user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :items
-  has_many :orders, through: :item
+  has_many :orders
+  has_many :items, through: :orders
 
   validates :username, uniqueness: true
   has_secure_password
