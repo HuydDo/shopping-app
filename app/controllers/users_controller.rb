201e2e3c965d09545621b9f9500a50e15e8c800f
HuydDo <huydinhdo@yahoo.com>
2019-12-04 03:53:04 -0700

    modify users controller, new view, index.css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      session[:user_id] = @user.user_id
+      session[:user_id] = @user.id
       redirect_to items_path
     else
       render :new
