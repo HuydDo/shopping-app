a165e7e5187642e39a6c0e5862182bb50937554c
HuydDo <huydinhdo@yahoo.com>
2019-12-04 03:31:47 -0700

    update users controller, add new view,  update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.user_id
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end
+
 end
