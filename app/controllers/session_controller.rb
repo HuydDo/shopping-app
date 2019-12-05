0b8a8b2f5eb509fa8d7edcd93225f2b28ba7a9f4
HuydDo <huydinhdo@yahoo.com>
2019-12-05 14:48:15 -0700

    modify application and session controller, new view

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -13,7 +13,9 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    session.delete :user_id
-    redirect_to root_url
+    if current_user
+      session.delete :user_id
+      redirect_to root_url
+    end
   end
 end
