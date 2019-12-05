a95c535931412f1ff892102c6479f4c855c68ae8
HuydDo <huydinhdo@yahoo.com>
2019-12-05 13:46:19 -0700

    modify session ctrler, routes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,10 +3,17 @@ class SessionController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(:name => params[:user][:name])
+    if user && user.aunthenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    session.delete :user_id
+    redirect_to root_url
   end
 end
