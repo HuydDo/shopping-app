31c663d5c6ab4cfeeba19a9d1525faac89833bc3
HuydDo <huydinhdo@yahoo.com>
2019-12-06 16:38:54 -0700

    modify crtler, model for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,8 @@ class OrdersController < ApplicationController
     # raise params.inspect
     order = Order.create(user_id: current_user.id, item_id: params[:item_id] )
     if order
-      respond = ride.order_item
-      flash[:notice] = reponse
+      response = order.order_item
+      flash[:notice] = response
       redirect_to user_path(order.user)
     else
       redirect_to item_path(order.item)
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :item
   belongs_to :user
+
+  def order_item
+    "Thanks for ordering the #{self.item.name}!"
+  end
 end
