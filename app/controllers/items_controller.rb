98831cde60d60be3d71332f43654d20aa9c15a9f
HuydDo <huydinhdo@yahoo.com>
2019-12-06 12:09:58 -0700

    modify controller, views for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,29 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+
+  end
+
+  def create
+    
+  end
+
   def show
   end
-  
+
+  def edit
+  end
+
+  def update
+  end
+
+  private 
+  def find_item
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def item_params
+    params.requires(:itme).permit(:name, :price, :description)
+  end
 end
