79543eecde18069504d33be6a60b1064479b8747
HuydDo <huydinhdo@yahoo.com>
2019-12-06 15:38:48 -0700

    modify order_controller, routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,14 @@ class OrdersController < ApplicationController
   before_action :require_login
 
   def create
-    raise order_params.inspect
-    order = Order.create(user_id: current_user.id, item_id: params[:attraction_id] )
-    redirect_to item_path(order.item)
+    # raise params.inspect
+    order = Order.create(user_id: current_user.id, item_id: params[:item_id] )
+    if order
+      respond = ride.order_item
+      flash[:notice] = reponse
+      redirect_to user_path(order.user)
+    else
+      redirect_to item_path(order.item)
+    end
   end
 end
