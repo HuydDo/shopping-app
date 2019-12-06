33ae098f9eee179973c641029cd3cefa93981f9c
HuydDo <huydinhdo@yahoo.com>
2019-12-05 20:48:16 -0700

    add home_pages controller, application stylesheets, home_page view, update appliction view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root "static_pages#home"
+  root "home_pages#home"
   resources :items
   # resources :users, except: [:new]
   resources :users, only: [:new, :create, :show]
