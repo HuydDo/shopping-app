71a751b8aa44db56808d48b1f48b74d54fc438ec
HuydDo <huydinhdo@yahoo.com>
2019-12-10 15:03:21 -0700

    update model for order, user, view for item and user

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,5 @@ class Order < ApplicationRecord
   belongs_to :user
   validates :quantity, presence: true
 
-  # def order_item
-  #   "Thanks for ordering the #{self.item.name}! 
-  #   Subtotal( # items): $#{total_amount}"
-  # end
-
-  # def total_amount
-  #   self.update(:quantity => self.quantity)
-  #   total = (self.quantity).to_f * self.item.price
-  # end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
 
   validates :name, uniqueness: true
   has_secure_password
+
+  # def order_item
+  #   "Thanks for ordering the #{self.item.name}! 
+  #   Subtotal( # items): $#{total_amount}"
+  # end
+
+  # def total_amount
+  #   self.update(:quantity => self.quantity)
+  #   total = (self.quantity).to_f * self.item.price
+  # end
 end
