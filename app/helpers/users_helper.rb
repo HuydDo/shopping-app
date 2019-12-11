264dc5e8b0672868486cc7e0c89368ce27c4cdd4
HuydDo <huydinhdo@yahoo.com>
2019-12-10 21:30:50 -0700

    update user helper, order and user views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,13 +3,9 @@ module UsersHelper
     item_count = 0
     subtotal = 0.0
     user.orders.each do |order| 
-      
         item_count += 1
         subtotal += order.item.price * order.quantity
     end
     render partial: 'subtotal', locals: {item_count: item_count, subtotal: subtotal}
   end
-
- 
-
 end
