b7141d2eb4add6447163dcabc0adad218b746f1e
HuydDo <huydinhdo@yahoo.com>
2019-12-14 15:26:16 -0700

    update helper and view for item

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -7,7 +7,7 @@ module ItemsHelper
     review_avg = 0.0
     item.orders.each do |item| 
         review_count += 1
-        subtotal += item.order.review
+        # subtotal += item.order.review
     end
     review_avg = subtotal / review_count
     render partial: 'review', locals: {review_count: review_count, review_avg: review_avg}
