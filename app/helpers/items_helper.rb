bc64c89e61c716978a33c7f46b562a43f287fc09
HuydDo <huydinhdo@yahoo.com>
2019-12-14 15:16:20 -0700

    update view for item, layout, orsder, and user.  update item helper.  create partial file for item view

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,15 @@
 module ItemsHelper
+  def review(item)
+  # byebug        
+
+    review_count = 0
+    subtotal = 0.0
+    review_avg = 0.0
+    item.orders.each do |item| 
+        review_count += 1
+        subtotal += item.order.review
+    end
+    review_avg = subtotal / review_count
+    render partial: 'review', locals: {review_count: review_count, review_avg: review_avg}
+  end
 end
