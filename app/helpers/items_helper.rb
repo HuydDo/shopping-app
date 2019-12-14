438bb9ad3535b4b65fb6f39a079776dda159fd3c
HuydDo <huydinhdo@yahoo.com>
2019-12-14 16:32:17 -0700

    update helper, veiw for item

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,14 +2,17 @@ module ItemsHelper
   def review(item)
   # byebug        
 
-    review_count = 0
-    subtotal = 0.0
-    review_avg = 0.0
-    item.orders.each do |item| 
-        review_count += 1
-        # subtotal += item.order.review
-    end
-    review_avg = subtotal / review_count
-    render partial: 'review', locals: {review_count: review_count, review_avg: review_avg}
+  review_count = 0
+  subtotal = 0.0
+  review_avg = 0.0
+  
+  @item.orders.each do |order|
+    review_count += 1 
+    subtotal += order.review if !order.review.nil?
+  end
+  
+  review_avg = subtotal / review_count
+  
+  render partial: 'review', locals: {review_count: review_count, review_avg: review_avg}
   end
 end
