6713762f1944711317609f9c9a8282d476df4a19
HuydDo <huydinhdo@yahoo.com>
2019-12-16 09:49:53 -0700

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,22 @@ class Item < ApplicationRecord
   
 
   scope :most_orders, -> {(
-    select("items.name, count(orders.id) as orders_count")
+    select("items.name, sum(orders.quantity) as orders_sum")
     .joins(:orders)
     .group("items.id")
-    .order("orders_count DESC")
+    .order("orders_sum DESC")
     .limit(1)
     )}
 
+  scope :most_reviews, -> {(
+  select("items.name, sum(orders.review) as reviews_sum")
+  .joins(:orders)
+  .group("items.id")
+  .order("reviews_sum DESC")
+  .limit(1)
+  )}
+
+  
     has_many :orders
   has_many :users, through: :orders
 end
