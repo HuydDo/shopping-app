facf88b1666213d7fb4198574f0a13568ebc2a67
HuydDo <huydinhdo@yahoo.com>
2019-12-16 00:07:47 -0700

    update model, view for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,7 @@
 class Item < ApplicationRecord
   scope :most_orders, -> {all.order(orders: :DESC).limit(1).first}
-  scope :Seller_recomend, -> {all.order(orders: :DESC).limit(1).first}
+  scope :seller_choice, -> {all.order(reviews: :DESC).limit(1).first}
+  
   has_many :orders
   has_many :users, through: :orders
-
-  #show which item was order the most
-  
 end
