942ac766b930850ec050dcce2ee2a82dbe06be98
HuydDo <huydinhdo@yahoo.com>
2019-12-15 23:37:51 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :items, through: :orders
 
   validates :name, uniqueness: true
-  # validates :name, presence: true
+  validates :name, presence: true
   # validates :email, presence: true
   has_secure_password
 
