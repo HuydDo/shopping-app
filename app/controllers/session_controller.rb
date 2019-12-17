5684872dae3e3c47c9c0ae4a979eb6aa2fdb3cc3
HuydDo <huydinhdo@yahoo.com>
2019-12-17 11:23:01 -0700

    Update session_controller.rb

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,18 +8,15 @@ class SessionController < ApplicationController
         u.name = auth['info']['name']
         u.email = auth['info']['email']
         u.password = SecureRandom.hex
-     end
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user)
-      # render 'welcome/home'
     else
       user = User.find_by(:name => params[:user][:name])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect_to user_path(user)
-        # redirect_to items_path
       else
-        # redirect with flash message
         render :new
       end
     end
