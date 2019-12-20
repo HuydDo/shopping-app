e35f84ace270d70bfcdc20b47d21d0e51f04ef5e
HuydDo <huydinhdo@yahoo.com>
2019-12-19 21:34:49 -0700

    remove col from order migration

---
+++ db/migrate/20191220043338_remove_review_from_orders.rb
@@ -0,0 +1,6 @@
+class RemoveReviewFromOrders < ActiveRecord::Migration[6.0]
+  def change
+
+    remove_column :orders, :review, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_203637) do
+ActiveRecord::Schema.define(version: 2019_12_20_043338) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2019_12_11_203637) do
     t.integer "user_id"
     t.integer "item_id"
     t.integer "quantity"
-    t.integer "review"
     t.boolean "delivered", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
