62407f36bb479e0de4344f5c457272cb6581de68
HuydDo <huydinhdo@yahoo.com>
2019-12-26 21:14:51 -0700

    udpate controller and view for fullfillment

--- app/controllers/fullfillment_controller.rb
+++ app/controllers/fullfillment_controller.rb
@@ -1,14 +1,29 @@
 class FullfillmentController < ApplicationController
-  before_action :find_item, only: [:show]
+  before_action :find_item, only: [:show, :edit]
 
 
   def index
     @items = Item.all  
-    
   end
 
   def show
    
+  end
+  
+  def edit
+  
+  end
+
+  def update
+    @item.update(name: params[:item][:name],price: params[:item][:price])
+    flash[:notice] = "Order was updated"
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+    flash[:notice] = "Item was deleted"
+    redirect_to user_path(current_user)
   end
 
   private
